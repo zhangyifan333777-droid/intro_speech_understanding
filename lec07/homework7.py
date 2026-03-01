a585264296010a5b0fa9b3bd5369adf709464bc3
zhangyifan333777-droid <zhangyifan333777@gmail.com>
2026-03-01 13:08:51 +0900

    Implement major chord generation and spectral analysis

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -3,44 +3,45 @@ import numpy as np
 def major_chord(f, Fs):
     '''
     Generate a one-half-second major chord, based at frequency f, with sampling frequency Fs.
-
-    @param:
-    f (scalar): frequency of the root tone, in Hertz
-    Fs (scalar): sampling frequency, in samples/second
-
-    @return:
-    x (array): a one-half-second waveform containing the chord
-    
-    A major chord is three notes, played at the same time:
-    (1) The root tone (f)
-    (2) A major third, i.e., four semitones above f
-    (3) A major fifth, i.e., seven semitones above f
     '''
-    raise RuntimeError("You need to write this part")
+    N = int(Fs / 2)
+    n = np.arange(N)
+
+    f1 = f
+    f2 = f * np.power(2, 4/12)
+    f3 = f * np.power(2, 7/12)
+
+    x = (np.cos(2*np.pi*f1*n/Fs)
+       + np.sin(2*np.pi*f2*n/Fs)
+       + np.cos(2*np.pi*f3*n/Fs))
+
+    return x
+
 
 def dft_matrix(N):
     '''
     Create a DFT transform matrix, W, of size N.
-    
-    @param:
-    N (scalar): number of columns in the transform matrix
-    
-    @result:
-    W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
-           W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    W = np.exp(-1j * 2 * np.pi * k * n / N)
+    return W
+
 
 def spectral_analysis(x, Fs):
     '''
     Find the three loudest frequencies in x.
-
-    @param:
-    x (array): the waveform
-    Fs (scalar): sampling frequency (samples/second)
-
-    @return:
-    f1, f2, f3: The three loudest frequencies (in Hertz)
-      These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+    X = np.abs(np.fft.fft(x))
+    N = len(X)
+
+    freqs = np.fft.fftfreq(N, d=1/Fs)
+
+    mask = freqs > 0
+    freqs = freqs[mask]
+    X = X[mask]
+
+    idx = np.argsort(X)[-3:]
+    freqs_found = np.sort(freqs[idx])
+
+    return freqs_found[0], freqs_found[1], freqs_found[2]
