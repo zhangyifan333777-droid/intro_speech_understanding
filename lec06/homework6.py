6fd4e9a48634217ce3c3c69b735d0560bb7f4041
zhangyifan333777-droid <zhangyifan333777@gmail.com>
2026-03-01 13:02:26 +0900

    Implement frequency calculations in homework6.py

--- lec06/homework6.py
+++ lec06/homework6.py
@@ -3,42 +3,23 @@ import numpy as np
 def minimum_Fs(f):
     '''
     Find the lowest sampling frequency that would avoid aliasing for a pure tone at f Hz.
-    
-    @param:
-    f (scalar): frequency in Hz (cycles/second)
-    
-    @result:
-    Fs (scalar): the lowest sampling frequency (samples/second) that would
-    not cause aliasing at a tone of f Hz.
     '''
-    Fs = 0  # change this line
+    Fs = 2 * f
     return Fs
+
 
 def omega(f, Fs):
     '''
     Find the radial frequency (omega) that matches a given f and Fs.
-    
-    @param:
-    f (scalar): frequency in Hz (cycles/second)
-    Fs (scalar): sampling frequency in samples/second
-    
-    @result:
-    omega (scalar): radial frequency in radians/sample
     '''
-    omega = 0  # change this line
+    omega = 2 * np.pi * f / Fs
     return omega
+
 
 def pure_tone(omega, N):
     '''
     Create a pure tone of N samples at omega radians/sample.
-    
-    @param:
-    omega (scalar): radial frequency, samples/second
-    N (scalar): duration of the tone, in samples
-    
-    @result:
-    x (array): N samples from the signal cos(omega*n)
     '''
-    x = 0 # change this line
+    n = np.arange(N)
+    x = np.cos(omega * n)
     return x
-
