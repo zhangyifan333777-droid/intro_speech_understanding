2f09a971b81860cad9eabe9f4d5626ae9600a476
zhangyifan333777-droid <zhangyifan333777@gmail.com>
2026-03-01 14:39:45 +0900

    Implement voiced excitation and resonator functions

--- lec12/homework12.py
+++ lec12/homework12.py
@@ -2,53 +2,45 @@ import numpy as np
 
 def voiced_excitation(duration, F0, Fs):
     '''
-    Create voiced speeech excitation.
-    
-    @param:
-    duration (scalar) - length of the excitation, in samples
-    F0 (scalar) - pitch frequency, in Hertz
-    Fs (scalar) - sampling frequency, in samples/second
-    
-    @returns:
-    excitation (np.ndarray) - the excitation signal, such that
-      excitation[n] = -1 if n is an integer multiple of int(np.round(Fs/F0))
-      excitation[n] = 0 otherwise
+    Create voiced speech excitation.
     '''
-    raise RuntimeError("You need to write this part!")
+    T0 = int(np.round(Fs / F0))   # pitch period
+    excitation = np.zeros(duration)
+    excitation[::T0] = -1
+    return excitation
+
 
 def resonator(x, F, BW, Fs):
     '''
     Generate the output of a resonator.
-    
-    @param:
-    x (np.ndarray(N)) - the excitation signal
-    F (scalar) - resonant frequency, in Hertz
-    BW (scalar) - resonant bandwidth, in Hertz
-    Fs (scalar) - sampling frequency, in samples/second
-    
-    @returns:
-    y (np.ndarray(N)) - resonant output
     '''
-    raise RuntimeError("You need to write this part!")
+    C = -np.exp(-2 * np.pi * BW / Fs)
+    B = 2 * np.exp(-np.pi * BW / Fs) * np.cos(2 * np.pi * F / Fs)
+    A = 1 - B - C
 
-def synthesize_vowel(duration,F0,F1,F2,F3,F4,BW1,BW2,BW3,BW4,Fs):
+    y = np.zeros(len(x))
+    y[0] = A * x[0]
+    y[1] = A * x[1] + B * y[0]
+
+    for n in range(2, len(y)):
+        y[n] = A * x[n] + B * y[n-1] + C * y[n-2]
+
+    return y
+
+
+def synthesize_vowel(duration, F0, F1, F2, F3, F4,
+                     BW1, BW2, BW3, BW4, Fs):
     '''
-    Synthesize a vowel.
-    
-    @param:
-    duration (scalar) - duration in samples
-    F0 (scalar) - pitch frequency in Hertz
-    F1 (scalar) - first formant frequency in Hertz
-    F2 (scalar) - second formant frequency in Hertz
-    F3 (scalar) - third formant frequency in Hertz
-    F4 (scalar) - fourth formant frequency in Hertz
-    BW1 (scalar) - first formant bandwidth in Hertz
-    BW2 (scalar) - second formant bandwidth in Hertz
-    BW3 (scalar) - third formant bandwidth in Hertz
-    BW4 (scalar) - fourth formant bandwidth in Hertz
-    Fs (scalar) - sampling frequency in samples/second
-    
-    @returns:
-    speech (np.ndarray(samples)) - synthesized vowel
+    Synthesize a vowel using source-filter model.
     '''
-    raise RuntimeError("You need to write this part!")
+
+    # Step 1: Create excitation
+    excitation = voiced_excitation(duration, F0, Fs)
+
+    # Step 2: Cascade four resonators (formants)
+    y1 = resonator(excitation, F1, BW1, Fs)
+    y2 = resonator(y1, F2, BW2, Fs)
+    y3 = resonator(y2, F3, BW3, Fs)
+    y4 = resonator(y3, F4, BW4, Fs)
+
+    return y4
